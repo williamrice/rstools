75e29b4269fe789bebbc73679288ad459add9cfd
William Rice <william.rice192@gmail.com>
2019-02-11 20:24:14 -0500

    rename on the main file, coords file just shows me coords for manual setup

---
+++ coords.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+while True:
+    x, y = pyautogui.position()
+    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    print(positionStr, end='')
+    print('\b' * len(positionStr), end='', flush=True)
